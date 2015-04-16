a91568166b3dd68e4a32a6c8cdeecba96c8366dd
Jackson Pires <jackson.pires@gmail.com>
2015-04-16 13:58:48 -0300

    First Commit

---
+++ app/assets/javascripts/components/hello_world.js.jsx
@@ -0,0 +1,11 @@
+/** @jsx React.DOM */
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <div className='HelloWorld'>
+        Hello, world!
+      </div>
+      );
+  }
+});
+
